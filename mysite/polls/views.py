b43b54f42a194a8e1aaca032a0f98947f6799b5a
brunoqs <brun_o13@hotmail.com>
2017-08-02 20:16:32 -0300

    lerigoooo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from .models import Post
+from .models import Post # importando bd 
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return HttpResponse(Post.objects.all())
-	
+	return render(request, 'polls/post_list.html', {})	
