600fd2cd4c8947f5c5e034b642b879ed9d4bd42f
brunoqs <brun_o13@hotmail.com>
2017-08-02 20:53:00 -0300

    Bd integrado html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
 from .models import Post # importando bd 
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-	return render(request, 'polls/post_list.html', {})	
+	autor = User.objects.get(username="admin")
+	posts = Post.objects.filter(autor=autor)
+	return render(request, 'polls/post_list.html', {'autor': autor,
+													'posts': posts})	
